4bd9ab0869a84ad0451b61c0b631a35170fc96c5
fedosovmu <fedosovmu@gmail.com>
2020-07-02 08:09:41 +0500

    Тестовый бот полностью изолирован от продакшена

--- flask_app.py
+++ flask_app.py
@@ -1,13 +1,19 @@
 from flask import Flask, request
 from telegram import Update
-from coffee_bot import CoffeeBot as TestCoffeeBot
-from config import TG_TOKEN as TEST_TG_TOKEN
+try:
+    from coffee_bot import CoffeeBot as TestCoffeeBot
+    from config import TG_TOKEN as TEST_TG_TOKEN
+except:
+    print('TEST BOT EXEPTION: import coffe_bot, config')
 from prodaction.coffee_bot import CoffeeBot as ProdactionCoffeeBot
 from prodaction.config import TG_TOKEN as PRODACTION_TG_TOKEN
 
 
 prodaction_bot = ProdactionCoffeeBot()
-test_bot = TestCoffeeBot()
+try:
+    test_bot = TestCoffeeBot()
+except:
+    print('TEST BOT EXEPTION: create TestCoffeeBot')
 app = Flask(__name__)
 
 
@@ -23,10 +29,12 @@ def prodaction_bot_webhook_receive_update():
         prodaction_bot.process_update(update)
     return '{"ok": True}'
 
-
-@app.route('/'+'1374409729:AAGfeRsaweSWtDf91ZnSDLYN1PynbsfYHzY', methods=["GET", "POST"])
-def test_bot_webhook_receive_update():
-    if request.method == "POST":
-        update = Update.de_json(request.get_json(), test_bot.bot)
-        test_bot.process_update(update)
-    return '{"ok": True}'
+try:
+    @app.route('/'+TEST_TG_TOKEN, methods=["GET", "POST"])
+    def test_bot_webhook_receive_update():
+        if request.method == "POST":
+            update = Update.de_json(request.get_json(), test_bot.bot)
+            test_bot.process_update(update)
+        return '{"ok": True}'
+except:
+    print('TEST BOT EXEPTION: test_bot_webhook_receive_update')
