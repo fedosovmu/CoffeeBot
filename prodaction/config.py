d64646f8029f4ce7533fe68adb8c23919d5c1fd5
fedosovmu <fedosovmu@gmail.com>
2020-08-21 17:36:24 +0300

    update configs

--- prodaction/config.py
+++ prodaction/config.py
@@ -1,14 +1,14 @@
 import app_mode
 
 
-TG_TOKEN = '1374409729:AAGfeRsaweSWtDf91ZnSDLYN1PynbsfYHzY'
+TG_TOKEN = '123'
 DB_HOST = 'mocoronco.mysql.pythonanywhere-services.com'
 DB_USER = 'mocoronco'
 DB_PASSWORD = '77777778py'
 DB_NAME = 'mocoronco$bot'
 ADMIN_CHAT_ID = '353684540'
 
-PRODACTION_TG_TOKEN = '1101055853:AAGU7qr9MTpD0OCDr-QLf73pzGQvTfe2sQQ'
+PRODACTION_TG_TOKEN = '321'
 PRODACTION_DB_NAME = 'mocoronco$prodaction_bot'
 
 if app_mode.is_prodaction(__file__):
--- test/config.py
+++ test/config.py
@@ -1,16 +1,19 @@
 import app_mode
 
-
-TG_TOKEN = '1374409729:AAGfeRsaweSWtDf91ZnSDLYN1PynbsfYHzY'
 DB_HOST = 'mocoronco.mysql.pythonanywhere-services.com'
 DB_USER = 'mocoronco'
 DB_PASSWORD = '77777778py'
-DB_NAME = 'mocoronco$bot'
 ADMIN_CHAT_ID = '353684540'
 
-PRODACTION_TG_TOKEN = '1101055853:AAGU7qr9MTpD0OCDr-QLf73pzGQvTfe2sQQ'
+TEST_TG_TOKEN = '123'
+TEST_DB_NAME = 'mocoronco$bot'
+
+PRODACTION_TG_TOKEN = '321'
 PRODACTION_DB_NAME = 'mocoronco$prodaction_bot'
 
 if app_mode.is_prodaction(__file__):
     TG_TOKEN = PRODACTION_TG_TOKEN
     DB_NAME = PRODACTION_DB_NAME
+else:
+    TG_TOKEN = TEST_TG_TOKEN
+    DB_NAME = TEST_DB_NAME
